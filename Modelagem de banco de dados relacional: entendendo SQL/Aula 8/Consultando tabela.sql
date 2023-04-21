a16a61ada0dc246bc4544ac2fa03d88880bb104f
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-04-21 16:36:56 -0300

    Create Consultando tabela.sql

---
+++ Modelagem de banco de dados relacional: entendendo SQL/Aula 8/Consultando tabela.sql
@@ -0,0 +1,5 @@
+SELECT * FROM livros; # Imprime tudo na tabela livro
+
+SELECT nome_livro FROM livros; # Imprime só a coluna nome dos livros.
+
+SELECT autoria AS "autores" FROM livros; # 	Apelidando uma coluna para consultar com o apelido
