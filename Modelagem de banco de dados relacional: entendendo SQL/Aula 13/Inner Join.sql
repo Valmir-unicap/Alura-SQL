f83c2960f0fc7517756b31bf8fbd0bfcb0bf77fb
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-04-21 19:00:42 -0300

    Update Inner Join.sql

--- Modelagem de banco de dados relacional: entendendo SQL/Aula 13/Inner Join.sql
+++ Modelagem de banco de dados relacional: entendendo SQL/Aula 13/Inner Join.sql
@@ -1,3 +1,5 @@
+# O INNER JOIN é a interseção entre duas tabelas, ou seja, na consulta aparecerá todas as informações de um determinado campo da tabela A que também foi encontrado na tabela B.
+
 SELECT vendas.ID_vendedor, vendedores.ID_vendedor, 
 SUM(vendas.quantidade_venda)
 FROM vendas INNER JOIN vendedores
