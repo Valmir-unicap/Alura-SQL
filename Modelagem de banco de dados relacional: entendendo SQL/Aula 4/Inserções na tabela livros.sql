38057a714b321ab33ef1ea31b58dd6be38aff0bb
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-04-21 15:24:20 -0300

    Create Inserções na tabela livros.sql

---
+++ Modelagem de banco de dados relacional: entendendo SQL/Aula 4/Inserções na tabela livros.sql
@@ -0,0 +1,10 @@
+SET FOREIGN_KEY_CHECKS = 0 ; # Ambas não estão mais relacionadas, e chave estrangeira foi desativada.
+
+INSERT INTO livros VALUES(
+1,
+"Harry Potter e a pedra filosofal",
+"J.k Rowlings",
+"Rocco",
+"Ficção científica",
+"26.99"
+);
