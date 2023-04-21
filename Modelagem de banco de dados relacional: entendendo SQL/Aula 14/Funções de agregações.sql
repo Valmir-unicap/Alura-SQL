7672e83bb23aadb64b1ff60094873f29baba0b28
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-04-21 18:54:49 -0300

    Create Funções de agregações.sql

---
+++ Modelagem de banco de dados relacional: entendendo SQL/Aula 14/Funções de agregações.sql
@@ -0,0 +1,6 @@
+
+SELECT MAX(quantidade_venda) FROM vendas; # Consulta o maior valor na tabela quantidade
+SELECT MIN(quantidade_venda) FROM vendas; # Consulta o menor valor na tabela quantidade
+SELECT AVG(quantidade_venda) FROM vendas; # Realiza a média aritmética dos valores de uma determinada coluna
+SELECT SUM(quantidade_venda) FROM vendas; # Calcula o somatório dos valores de um campo específico;
+SELECT COUNT(quantidade_venda) FROM vendas; # Contabiliza a quantidade de linhas selecionadas
