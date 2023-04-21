cdbe1f7a496c66e1356c00f497cd96bb4ad6885c
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-04-21 17:29:47 -0300

    Create Alterando informações da tabela.sql

---
+++ Modelagem de banco de dados relacional: entendendo SQL/Aula 11/Alterando informações da tabela.sql
@@ -0,0 +1,5 @@
+SELECT * FROM livros;
+
+DELETE FROM livros WHERE ID_livro = 13; # Deletando uma linha da tabela usando ID.
+
+UPDATE livros SET preco = 0.9*preco; # Aplica desconto de 90% na tabela preço.
