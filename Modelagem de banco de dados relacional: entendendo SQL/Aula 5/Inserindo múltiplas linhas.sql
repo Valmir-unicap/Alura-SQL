53d365cbd9b838eb38211b1f65d6c5c7ff3db680
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-04-21 15:52:24 -0300

    Create Inserindo múltiplas linhas.sql

---
+++ Modelagem de banco de dados relacional: entendendo SQL/Aula 5/Inserindo múltiplas linhas.sql
@@ -0,0 +1,11 @@
+INSERT INTO livros VALUES
+(9, "Divergente", "Veronica Roth", "Rocco", "Ficção científica", 23.99),
+(10, "Convergente", "Veronica Roth", "Rocco", "Ficção científica", 24.99),
+(11, "Insurgente", "Veronica Roth", "Rocco", "Ficção científica", 27.99),
+(12, "Quatro", "Veronica Roth", "Rocco", "Ficção científica", 28.99);
+
+INSERT INTO vendedores VALUES
+(1, "Magazine Luiza"),
+(2, "Saraiva"),
+(3, "Amazon"),
+(4, "Lojas Americanas");
