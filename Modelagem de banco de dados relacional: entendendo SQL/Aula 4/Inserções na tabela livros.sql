1cfed5ec3dc7c42cd84d2e4a463cdf1e7d6adc8d
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-04-21 15:31:38 -0300

    Update Inserções na tabela livros.sql

--- Modelagem de banco de dados relacional: entendendo SQL/Aula 4/Inserções na tabela livros.sql
+++ Modelagem de banco de dados relacional: entendendo SQL/Aula 4/Inserções na tabela livros.sql
@@ -3,8 +3,71 @@ SET FOREIGN_KEY_CHECKS = 0 ; # Ambas não estão mais relacionadas, e chave estr
 INSERT INTO livros VALUES(
 1,
 "Harry Potter e a pedra filosofal",
-"J.k Rowlings",
+"J.k Rowling",
 "Rocco",
 "Ficção científica",
 "26.99"
+);
+
+INSERT INTO livros VALUES(
+2,
+"Harry Potter e a camera secreta",
+"J.k Rowling",
+"Rocco",
+"Ficção científica",
+"25.99"
+);
+
+INSERT INTO livros VALUES(
+3,
+"Harry Potter e o prisioneiro de Azkaban",
+"J.k Rowling",
+"Rocco",
+"Ficção científica",
+"23.99"
+);
+
+INSERT INTO livros VALUES(
+4,
+"Harry Potter e o calice de fogo",
+"J.k Rowling",
+"Rocco",
+"Ficção científica",
+"24.99"
+);
+
+INSERT INTO livros VALUES(
+5,
+"Harry Potter e a ordem da fenix",
+"J.k Rowling",
+"Rocco",
+"Ficção científica",
+"25.99"
+);
+
+INSERT INTO livros VALUES(
+6,
+"Harry Potter e o enigma do princípe",
+"J.k Rowling",
+"Rocco",
+"Ficção científica",
+"26.99"
+);
+
+INSERT INTO livros VALUES(
+7,
+"Harry Potter e as reliquias da morte Parte 1",
+"J.k Rowling",
+"Rocco",
+"Ficção científica",
+"27.99"
+);
+
+INSERT INTO livros VALUES(
+8,
+"Harry Potter e as reliquias da morte Parte 2",
+"J.k Rowling",
+"Rocco",
+"Ficção científica",
+"28.99"
 );
